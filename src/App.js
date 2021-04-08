0c6e28bb1d290cea9f5004915edd1d080211de62
s-as-as100 <s.arifsaifi000@gmail.com>
2021-04-09 02:13:15 +0530

    make a header component nd style the header comp

---
+++ src/App.js
@@ -0,0 +1,21 @@
+ import React from 'react';
+  
+ 
+
+import './App.css';
+import Header from './components/header/header';
+
+ class App extends React.Component {
+   render() {
+     return (
+       <React.Fragment>
+         <Header/>
+       </React.Fragment>
+     )
+   }
+ }
+  
+     
+  
+
+export default App;
